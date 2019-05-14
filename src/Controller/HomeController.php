4f110fa6cc8c6323e6b06a066e2ed9b49994d1b0
Cedric <cedric.chape@modis.com>
2019-05-14 16:04:56 +0200

    Created first pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,18 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
-{
+class HomeController extends AbstractController
+{    
     /**
-     * @var Environment
+     * @Route("/", name="home")
+     * @return Response
      */
-    private $twig;
-    
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;    
-    }
-    
-    
     public function index(): Response
     {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
     }
 }
