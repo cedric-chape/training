bfb85dc0171795595418aef97f0774d2270a85f5
Cedric <cedric.chape@modis.com>
2019-05-17 11:07:42 +0200

    Validators

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -3,10 +3,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- *
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -25,7 +27,7 @@ class Property
     private $id;
 
     /**
-     *
+     * @Assert\Length(min=5, max=255)
      * @ORM\Column(type="string", length=255)
      */
     private $title;
@@ -37,8 +39,8 @@ class Property
     private $description;
 
     /**
-     *
      * @ORM\Column(type="integer")
+     * @Assert\Range(min=10, max=400)
      */
     private $surface;
 
@@ -85,8 +87,8 @@ class Property
     private $address;
 
     /**
-     *
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postal_code;
 
